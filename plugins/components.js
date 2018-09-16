1d4116b762165f2b15199a2aea7d1b97c52f8a12
firs <firs058@gmail.com>
2018-09-16 23:50:54 +0500

    v 3.0.13

--- plugins/components.js
+++ plugins/components.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Calendar from '~/components/calendar'
 import Social from '~/components/social'
+import Loading from '~/components/loading'
 import Card from '~/components/card'
 import Comment from '~/components/comment'
 import CommentDialog from '~/components/commentDialog'
@@ -8,6 +9,7 @@ import CommentDialog from '~/components/commentDialog'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VueRecaptcha from 'vue-recaptcha'
 
+Vue.component('loading', Loading);
 Vue.component('calendar', Calendar);
 Vue.component('social', Social);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
