1da3865c77913f8a601fd244aebeac9028489e59
firs <firs058@gmail.com>
2019-11-17 03:57:26 +0500

    v 3.3.58

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,5 +1,5 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <v-container article :class="$device.isMobile ? 'grid-list-xs' : 'grid-list-lg pt-0'">
+    <v-container article :class="$device.isMobile ? 'grid-list-lg' : 'grid-list-lg pt-0'">
         <v-layout row wrap>
             <v-flex xs12>
                 <v-text-field
