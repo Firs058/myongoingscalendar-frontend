7ff9348178def365bf311187454762c18faf63cc
firs <firs058@gmail.com>
2020-05-29 20:27:00 +0500

    v 3.5.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
     head: {
         meta: [
             {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {name: 'google-site-verification', content: 'XYNXmJI5XPFQwvDE04hZnZ_NFN1p3ighYHzA0uvPmCI'}
         ],
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/images/icons/favicon.ico'}
