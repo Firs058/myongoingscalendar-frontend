3294daf3fa77d22fd10902ccd8bd3eb16c0f8749
firs <firs058@gmail.com>
2020-10-01 16:20:34 +0500

    v 3.6.0.33

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'myongoingscalendar-frontend',
             exec_mode: 'cluster',
             instances: 'max',
-            cwd: './.nuxt',
+            cwd: '/opt/app/myongoingscalendar-frontend/.nuxt',
             script: "./node_modules/nuxt/bin/nuxt.js",
             args: "start",
         },
