98722c111a05ca1fc0999717e501e8e84ec92e4a
firs <firs058@gmail.com>
2020-10-01 12:01:10 +0500

    v 3.6.0.18

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     apps: [
         {
-            name: 'app',
+            name: 'myongoingscalendar-frontend',
             exec_mode : 'cluster',
             instances: 'max',
-            cwd: './dist',
             script: "/usr/local/lib/node_modules/nuxt/bin/nuxt.js",
             args: "start",
         },
@@ -30,18 +29,5 @@ module.exports = {
                 followSymlinks: false
             }
         }
-    ],
-
-    deploy: {
-        production: {
-            user: 'SSH_USERNAME',
-            host: 'SSH_HOSTMACHINE',
-            ref: 'origin/master',
-            repo: 'GIT_REPOSITORY',
-            path: 'DESTINATION_PATH',
-            'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-            'pre-setup': ''
-        }
-    }
+    ]
 };
