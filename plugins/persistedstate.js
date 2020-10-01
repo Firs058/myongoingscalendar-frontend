af3d6f7c3abb57d426062982f81eac0eabfcb319
firs <firs058@gmail.com>
2020-10-01 22:14:03 +0500

    v 3.6.0.41

--- plugins/persistedstate.js
+++ plugins/persistedstate.js
@@ -1,12 +1,15 @@
 import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie';
+import cookie from 'cookie'
+import {encode, decode} from 'js-base64';
 
-export default ({app, store}) => {
+export default ({store, req}) => {
     createPersistedState({
         paths: ['tokens', 'settings.guest', 'agreeWithTermsAndPolicy'],
         storage: {
-            getItem: (key) => app.$cookies.get(key),
-            setItem: (key, value) => app.$cookies.set(key, value),
-            removeItem: (key) => app.$cookies.remove(key)
+            getItem: (key) => decode(process.client ? Cookies.get(key) : cookie.parse(req.headers.cookie || '')[key]),
+            setItem: (key, value) => !Number.isInteger(value) ? Cookies.set(key, encode(value), {expires: 365}) : undefined,
+            removeItem: (key) => Cookies.remove(key)
         }
     })(store)
 }
