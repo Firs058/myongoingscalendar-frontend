5ca61ffbac0be6bc428bdc09cdb635db1eeb98a7
firs <firs058@gmail.com>
2020-10-01 11:29:12 +0500

    v 3.6.0.15

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    apps: [
+        {
+            name: 'MyOngoingsCalendar',
+            instances: 'max',
+            cwd: './dist',
+            script: "./node_modules/nuxt/bin/nuxt.js",
+            args: "start",
+        },
+        {
+            name: "vibrant",
+            script: 'vibrant.js',
+            ignore_watch: [
+                "node_modules",
+                "images"
+            ],
+            watch_options: {
+                followSymlinks: false
+            }
+        },
+        {
+            name: "webp",
+            script: 'webp.js',
+            ignore_watch: [
+                "node_modules",
+                "images"
+            ],
+            watch_options: {
+                followSymlinks: false
+            }
+        }
+    ],
+
+    deploy: {
+        production: {
+            user: 'SSH_USERNAME',
+            host: 'SSH_HOSTMACHINE',
+            ref: 'origin/master',
+            repo: 'GIT_REPOSITORY',
+            path: 'DESTINATION_PATH',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
+    }
+};
