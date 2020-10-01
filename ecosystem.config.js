9c9bb2dd6cd72f1aea7f9f4a37edc8ec9e28294e
firs <firs058@gmail.com>
2020-10-01 17:32:38 +0500

    v 3.6.0.38

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,12 @@ module.exports = {
             cwd: '/opt/app/myongoingscalendar-frontend',
             script: "./node_modules/nuxt/bin/nuxt.js",
             args: "start",
+            env: {
+                NODE_ENV: 'production'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            },
             ignore_watch: [
                 "node_modules"
             ],
