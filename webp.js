8a5bb4d1fc1b6016b17f17612597a4a695594ccb
firs <firs058@gmail.com>
2021-03-27 17:21:34 +0500

    v 3.8.4

--- webp.js
+++ webp.js
@@ -10,12 +10,8 @@ app.post('/', (req, res) => {
   const input = data.file;
   const output = data.to;
 
-  webp.cwebp(input, output, `-q ${data.quality} -mt`, (status, error) => {
-    if (Number(status) === 100)
-      res.status(200).send({ status: Number(status), message: `Image converted: ${output}` });
-    else
-      res.status(200).send({ status: Number(status), message: error });
-  });
+  webp.cwebp(input, output, `-q ${data.quality} -mt`, logging = '-v')
+    .then((status) => res.status(200).send({ status: Number(200), message: status }));
 });
 
 app.listen(8085);
